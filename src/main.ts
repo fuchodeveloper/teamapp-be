d421b9c48ed41d6eab2069cbcf1ebc0fcad1d76b
fuchodeveloper <fmgbeoma@yahoo.com>
2020-03-15 22:16:00 +0100

    BE scaffolding

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
-console.log('wassup');
-// Hot Module Replacement
+import { ApolloServer } from 'apollo-server';
+
+import resolvers from './resolvers';
+import typeDefs from './typeDef';
+import { environment } from './environment';
+
+const server = new ApolloServer({ 
+  resolvers, 
+  typeDefs,
+  introspection: environment.apollo.introspection,
+  playground: environment.apollo.playground
+});
+
+server.listen(environment.port)
+  .then(({ url }) => console.log(`server ready at ${url}`));
+
 if (module.hot) {
   module.hot.accept();
-  module.hot.dispose(() => console.log('Module disposed. '));
-}
+  module.hot.dispose(() => server.stop());
+};
