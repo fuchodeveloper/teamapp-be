80c1a0487eb04f8a4c06bbd9926b9467d2cf96b8
fuchodeveloper <fmgbeoma@yahoo.com>
2020-04-24 21:21:00 +0100

    domain

--- src/resolvers/userResolver.ts
+++ src/resolvers/userResolver.ts
@@ -39,7 +39,7 @@ export default {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-            domain: 'https://teamapp-fe.now.sh', //set your domain
+            // domain: 'https://teamapp-fe.now.sh', //set your domain
             SameSite: 'None',
           });
 
