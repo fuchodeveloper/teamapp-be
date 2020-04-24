f2f86288e04c0077127c0044aa5c389c39654435
fuchodeveloper <fmgbeoma@yahoo.com>
2020-04-24 17:59:50 +0100

    cors

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,7 @@ const startServer = async () => {
     credentials: true,
   };
   const app = express();
-  app.use(cors(corsConfig));
+  app.use(cors(corsConfig2));
   app.use(cookieParser());
 
   const server = new ApolloServer({
@@ -72,7 +72,7 @@ const startServer = async () => {
 
   server.applyMiddleware({
     app,
-    cors: true,
+    cors: false,
   });
 
   await mongoose.connect(environment.database, {
